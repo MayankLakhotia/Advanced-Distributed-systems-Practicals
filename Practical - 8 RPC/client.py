a9481b82823e2ab680db5eecb59a175c3bf8f27a
Mayank Lakhotia <65710263+MayankLakhotia@users.noreply.github.com>
2022-11-22 00:02:47 +0530

    Add files via upload

---
+++ Practical - 8 RPC/client.py
@@ -0,0 +1,16 @@
+import xmlrpc.client
+
+server = xmlrpc.client.ServerProxy('http://localhost:8000')
+
+x = input("Please enter x : ")
+y = input("Enter y : ")
+
+# print(s.pow(int(x),int(y)))  # Returns x*y = 8
+print(x,"+",y,"=",server.add(int(x),int(y)))  # Returns x+y 
+print(x,"-",y,"=",server.subtract(int(x),int(y))) # Return x-y
+print(x,"*",y,"=",server.mul(int(x),int(y)))  # Returns x*y
+print(x,"//",y,"=",server.divide(int(x),int(y))) #Return x // y
+
+
+# Print list of available methods
+# print(s.system.listMethods())
